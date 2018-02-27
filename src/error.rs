3b0e67748bec2223f9d242046a74b1ce31f0c14a
Zach Shipko <zachshipko@gmail.com>
2018-02-26 22:10:01 -0800

    update

--- src/error.rs
+++ src/error.rs
@@ -10,6 +10,20 @@ pub enum Error {
 
 impl From<lmdb::Error> for Error {
     fn from(err: lmdb::Error) -> Error {
-        Error::LMDB(err)
+        match err {
+            lmdb::Error::NotFound => Error::NotFound,
+            lmdb::Error::BadDbi => Error::InvalidBucket,
+            _ => Error::LMDB(err)
+        }
+
+    }
+}
+
+impl Error {
+    pub fn key_exists_error(&self) -> bool {
+        match self {
+            &Error::LMDB(lmdb::Error::KeyExist) => true,
+            _ => false
+        }
     }
 }
