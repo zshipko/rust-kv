b14f07e8c5779174bc372c0ce0efb7899fd1c30b
Zach Shipko <zachshipko@gmail.com>
2018-03-04 22:12:08 -0800

    Fix test

--- src/test.rs
+++ src/test.rs
@@ -99,7 +99,7 @@ fn test_cbor_encoding() {
     txn.commit().unwrap();
 
     let txn = store.read_txn::<ValueBuf<Cbor>>().unwrap();
-    let v = txn.get(bucket, "testing").unwrap().inner();
+    let v = txn.get(bucket, "testing").unwrap().inner().unwrap();
     assert_eq!(v.as_boolean().unwrap(), true);
     txn.abort();
 }
