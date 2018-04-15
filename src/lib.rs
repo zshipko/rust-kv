37c1e9036977c7cc2785afc5ee039969ac08704b
asonix <asonix.dev@gmail.com>
2018-04-15 17:29:59 -0500

    Don't unwrap in frontpage docs

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,9 @@
 //! ```rust
 //! extern crate kv;
 //!
-//! use kv::{Config, Manager, ValueRef};
+//! use kv::{Config, Error, Manager, ValueRef};
 //!
-//! fn main() {
+//! fn run() -> Result<(), Error> {
 //!     // First step create a manager, this ensured that each LMDB environment will only be
 //!     // accessed once per process
 //!     let mut mgr = Manager::new();
@@ -22,36 +22,42 @@
 //!     cfg.bucket("test", None);
 //!
 //!     // Get a Store handle
-//!     let handle = mgr.open(cfg).unwrap();
+//!     let handle = mgr.open(cfg)?;
 //!
 //!     // Get read-write access to the underlying store
-//!     let store = handle.write().unwrap();
+//!     let store = handle.write()?;
 //!
 //!     // A Bucket provides typed access to an LMDB database
-//!     let bucket = store.bucket::<&str, &str>(Some("test")).unwrap();
+//!     let bucket = store.bucket::<&str, &str>(Some("test"))?;
 //!
 //!     {
 //!         // Finally, a transaction is needed, they can be read-write or readonly, here we will use a
 //!         // write transaction to add data
-//!         let mut txn = store.write_txn().unwrap();
+//!         let mut txn = store.write_txn()?;
 //!
 //!         // To set a value
-//!         let () = txn.set(&bucket, "testing", "abc123").unwrap();
+//!         let () = txn.set(&bucket, "testing", "abc123")?;
 //!
 //!         // Make sure to commit the transaction. There is also an `abort` function to abandon
 //!         // the transaction
-//!         txn.commit().unwrap();
+//!         txn.commit()?;
 //!     }
 //!
 //!     {
 //!         // This time a readonly transaction
-//!         let txn = store.read_txn().unwrap();
+//!         let txn = store.read_txn()?;
 //!
 //!         // Getting a value is easy once everything is set up
-//!         let val = txn.get(&bucket, "testing").unwrap();
+//!         let val = txn.get(&bucket, "testing")?;
 //!         println!("testing => {}", val);
 //!     }
+//!
+//!     Ok(())
 //! }
+//! #
+//! # fn main() {
+//! #     run().unwrap();
+//! # }
 //! ```
 
 #[macro_use]
